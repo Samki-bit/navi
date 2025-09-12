29d7ea77e2b3ecaf1d2a104abe7e9548e800e8d0
Samki <ssameep5@gmail.com>
2025-09-12 20:12:33 +0545

    config files added and script to install nvim

---
+++ install.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+print(os.name)
+print("Let's install your config!")
+
+print("# Installation Options")
+print("[1] Complete Setup")
+print("[2] Nvim Setup")
+
+choice = input("Choose [1] [2] : ")
+
+match choice:
+    case "1":
+        print("\nhehe, its not done yet")
+
+    case "2":
+        print("\n# Installing neovim")
+        os.system("sudo pacman -S neovim")
+        print("\nNeovim installed")
+        print("\n Moving configs")
+        os.system("mv ~/navi/configs/nvim ~/.config")
+        print("\n Configs moved!")
+
+    case _:
+        print("Invalid, Please try again!")
+        sys.exit()
+
+print("\nInstallation Successfull!")
