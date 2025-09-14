f1f57074940344f1909de640fcdae43af42d1604
Samki <ssameep5@gmail.com>
2025-09-14 12:53:36 +0545

    install script for hyprland configs

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-print("Let's install your config!")
+print("Let's install your config! (FOR ARCH ONLY!)")
 
 print("# Installation Options")
 print("[1] Complete Setup")
@@ -11,14 +11,23 @@ choice = input("Choose [1] [2] : ")
 
 match choice:
     case "1":
-        print("\nhehe, its not done yet")
+        print("\n #Moving bashrc file")
+        os.system("cp -r rcfiles/.bashrc ")
+
+        print("\n# Installing packages")
+        os.system("sudo pacman -S - < packages.txt")
+        print("\nPackages installed successfully!")
+
+        print("\n#Moving configs")
+        os.system("cp -r configs/*/ ~/.config")
+        print("Configs moved!")
 
     case "2":
         print("\n# Installing neovim")
         os.system("sudo pacman -S neovim")
         print("\nNeovim installed")
         print("\n Moving configs")
-        os.system("cp -r ~/navi/configs/nvim ~/.config")
+        os.system("cp -r configs/nvim ~/.config")
         print("\n Configs moved!")
 
     case _:
